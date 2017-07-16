3153f31f855ed88e7df293ef6ffd2dfe22f2ce8e
i2391921 <i2391921@mvrht.net>
2017-07-16 09:03:32 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+//! Opens an empty window.
+
+extern crate amethyst;
+
+use amethyst::{Application, Event, State, Trans, VirtualKeyCode, WindowEvent};
+use amethyst::asset_manager::AssetManager;
+use amethyst::config::Element;
+use amethyst::ecs::World;
+use amethyst::gfx_device::DisplayConfig;
+use amethyst::renderer::Pipeline;
+
+struct Example;
+
+impl State for Example {
+    fn on_start(&mut self, _: &mut World, _: &mut AssetManager, pipe: &mut Pipeline) {
+        use amethyst::renderer::Layer;
+        use amethyst::renderer::pass::Clear;
+
+        let clear_layer = Layer::new("main", vec![Clear::new([1.0, 0.0, 0.0, 1.0])]);
+        pipe.layers = vec![clear_layer];
+    }
+
+    fn handle_events(&mut self,
+                     events: &[WindowEvent],
+                     _: &mut World,
+                     _: &mut AssetManager,
+                     _: &mut Pipeline)
+                     -> Trans {
+        for e in events {
+            match **e {
+                Event::KeyboardInput(_, _, Some(VirtualKeyCode::Escape)) => return Trans::Quit,
+                Event::Closed => return Trans::Quit,
+                _ => (),
+            }
+        }
+        Trans::None
+    }
+}
+
+fn main() {
+    let path = format!("{}/resources/config.yml",
+                       env!("CARGO_MANIFEST_DIR"));
+    let cfg = DisplayConfig::from_file(path).unwrap();
+    let mut game = Application::build(Example, cfg).done();
+    game.run();
+}
